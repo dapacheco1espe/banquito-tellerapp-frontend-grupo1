746cf2aaceba335a7c7105e63d1ed11d8ec8ed84
EliMorejon <memorejon@espe.edu.ec>
2023-07-23 06:49:18 -0500

    update

---
+++ src/app/modules/client/transferences/transferences.component.ts
@@ -0,0 +1,69 @@
+// import { Component } from '@angular/core';
+// import { Transferences } from './transferences.model';
+// import { TransferencesService } from './transferences.service';
+
+// @Component({
+//   selector: 'app-transferences',
+//   templateUrl: './transferences.component.html',
+//   styleUrls: ['./transferences.component.scss']
+// })
+// export class TransferencesComponent {
+//   cuentaId: number;
+//   monto: number;
+//   retiroExitoso: boolean = false;
+
+//   constructor(private transferencesService: TransferencesService) { }
+
+//   realizarRetiro(): void {
+//     if (this.cuentaId && this.monto && this.monto > 0) {
+//       this.transferencesService.realizarRetiro(this.cuentaId, this.monto).subscribe(
+//         () => {
+//           // El retiro se realizó con éxito
+//           this.retiroExitoso = true;
+//         },
+//         (error) => {
+//           console.error('Error al realizar el retiro:', error);
+//           this.retiroExitoso = false;
+//         }
+//       );
+//     } else {
+//       // Marcar el retiro como no exitoso si falta información o el monto es inválido
+//       this.retiroExitoso = false;
+//     }
+//   }
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { Transferences } from './transferences.model';
+import { TransferencesService } from './transferences.service';
+
+@Component({
+  selector: 'app-transferences',
+  templateUrl: './transferences.component.html',
+  styleUrls: ['./transferences.component.scss']
+})
+export class TransferencesComponent {
+  cuentaId: number;
+  monto: number;
+  transferenciaExitoso: boolean = false;
+
+  constructor(private transferencesService: TransferencesService) { }
+
+  realizarTransferencia(): void {
+    if (this.cuentaId && this.monto && this.monto > 0) {
+      this.transferencesService.realizarTransferencias(this.cuentaId, this.monto).subscribe(
+        () => {
+          // El retiro se realizó con éxito
+          this.transferenciaExitoso = true;
+        },
+        (error) => {
+          console.error('Error al realizar el retiro:', error);
+          this.transferenciaExitoso = false;
+        }
+      );
+    } else {
+      // Marcar el retiro como no exitoso si falta información o el monto es inválido
+      this.transferenciaExitoso = false;
+    }
+  }
+}
